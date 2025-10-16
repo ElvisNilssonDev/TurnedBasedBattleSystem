d578b1b0173b22aa8c11dd238dc5acd362a54805
Elvis Nilsson <nilssonelvis@gmail.com>
2025-10-16 11:10:00 +0200

    The entire skeleton of the program or aka the entire logical structure is completed. could implement better UI to the next uppdate

--- TurnedBasedBattleSystem/Program.cs
+++ TurnedBasedBattleSystem/Program.cs
@@ -8,15 +8,33 @@ namespace TurnedBasedBattleSystem
         {
             Unit player = new Unit(100, 20, 12, 10, "Player");
             Unit enemy = new Unit(80, 15, 10, 0, "Enemy");
+            Random random = new Random();
+            
+            while(!player.IsDead && !enemy.IsDead)
+            {
+                Console.WriteLine($"{player.UnitName} HP = {player.Hp}. {enemy.UnitName} HP {enemy.Hp}");
+                Console.WriteLine("Its the players turn! What will you do?!");
+                string choice = Console.ReadLine();
 
-            Console.WriteLine($"{player.UnitName} HP = {player.Hp}. {enemy.UnitName} HP {enemy.Hp}");
-            Console.WriteLine("Its the players turn! What will you do?!");
-            string choice = Console.ReadLine();
+                if (choice == "a")
+                    player.Attack(enemy);
+                else
+                    player.Heal();
 
-            if (choice == "a") 
-                player.Attack(enemy);
+                if (player.IsDead || enemy.IsDead) break;
 
-            Console.WriteLine($"{player.UnitName} HP = {player.Hp}. {enemy.UnitName} HP {enemy.Hp}");
+                Console.WriteLine($"{player.UnitName} HP = {player.Hp}. {enemy.UnitName} HP {enemy.Hp}");
+
+                Console.WriteLine("Its the enemys turn!");
+
+                int rand = random.Next(0, 2);
+
+                if (rand == 0)
+                    enemy.Attack(player);
+                else
+                    enemy.Heal();
+            }
+            
         }
     }
 }
